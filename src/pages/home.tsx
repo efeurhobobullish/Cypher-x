200a66d782434df5b8646018d83ebf6ae0c469a6
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-15 04:56:19 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 import AppLayout from "@/layouts/AppLayout";
-import { ModeToggle } from "@/components/ui";
+import  ModeToggle from "@/components/ui";
 import { Github, Rocket } from "lucide-react";
 
 export default function Home() {
